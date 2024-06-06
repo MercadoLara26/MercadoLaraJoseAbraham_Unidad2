df7c156817657fb15d13958ec89a8ecb4aff7c10
MercadoLara26 <mercadoabraham397@gmail.com>
2024-06-06 11:32:14 -0600

    Add files via upload

---
+++ JoseAbrahamMercadoLara_Unidad2/JoseAbrahamMercadoLara_Unidad2/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace JoseAbrahamMercadoLara_Unidad2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea6 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend6 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
+            this.lblActualizacion2 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel2 = new MetroFramework.Controls.MetroLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart2
+            // 
+            chartArea6.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea6);
+            legend6.Name = "Legend1";
+            this.chart2.Legends.Add(legend6);
+            this.chart2.Location = new System.Drawing.Point(232, 161);
+            this.chart2.Name = "chart2";
+            this.chart2.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Excel;
+            series6.ChartArea = "ChartArea1";
+            series6.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
+            series6.Legend = "Legend1";
+            series6.Name = "Series1";
+            this.chart2.Series.Add(series6);
+            this.chart2.Size = new System.Drawing.Size(300, 300);
+            this.chart2.TabIndex = 0;
+            this.chart2.Text = "GRAFICA";
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.Location = new System.Drawing.Point(284, 494);
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.Size = new System.Drawing.Size(139, 40);
+            this.metroButton1.TabIndex = 1;
+            this.metroButton1.Text = "Mostrar Grafica";
+            this.metroButton1.Click += new System.EventHandler(this.metroButton1_Click);
+            // 
+            // lblActualizacion2
+            // 
+            this.lblActualizacion2.AutoSize = true;
+            this.lblActualizacion2.Location = new System.Drawing.Point(549, 494);
+            this.lblActualizacion2.Name = "lblActualizacion2";
+            this.lblActualizacion2.Size = new System.Drawing.Size(87, 20);
+            this.lblActualizacion2.TabIndex = 4;
+            this.lblActualizacion2.Text = "metroLabel2";
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.Location = new System.Drawing.Point(23, 560);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(136, 20);
+            this.metroLabel1.TabIndex = 5;
+            this.metroLabel1.Text = "Dionisio Perez Perez";
+            this.metroLabel1.Click += new System.EventHandler(this.metroLabel1_Click);
+            // 
+            // metroLabel2
+            // 
+            this.metroLabel2.AutoSize = true;
+            this.metroLabel2.Location = new System.Drawing.Point(483, 560);
+            this.metroLabel2.Name = "metroLabel2";
+            this.metroLabel2.Size = new System.Drawing.Size(234, 20);
+            this.metroLabel2.TabIndex = 6;
+            this.metroLabel2.Text = "Topicos avanzados de programacion";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 591);
+            this.Controls.Add(this.metroLabel2);
+            this.Controls.Add(this.metroLabel1);
+            this.Controls.Add(this.lblActualizacion2);
+            this.Controls.Add(this.metroButton1);
+            this.Controls.Add(this.chart2);
+            this.Name = "Form1";
+            this.Text = "Mercado Lara Jose Abraham - Unidad 2";
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private MetroFramework.Controls.MetroButton metroButton1;
+        private MetroFramework.Controls.MetroLabel lblActualizacion2;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private MetroFramework.Controls.MetroLabel metroLabel2;
+    }
+}
+
---
+++ JoseAbrahamMercadoLara_Unidad2/JoseAbrahamMercadoLara_Unidad2/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace JoseAbrahamMercadoLara_Unidad2
+{
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+        Thread Hilo1;
+        Thread Hilo2;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void metroButton1_Click(object sender, EventArgs e)
+        {
+            Random rn = new Random();
+            int numeroRandom = rn.Next(1, 100);
+            int x = numeroRandom;
+            int y = 100 - numeroRandom;
+
+            if (chart2.InvokeRequired)
+            {
+                chart2.Invoke(new MethodInvoker(delegate
+                {
+                    chart2.Series.Clear();
+                    chart2.BackColor = Color.Transparent;
+                    Series serie1 = new Series
+                    {
+                        Name = "Grados",
+                        IsVisibleInLegend = false,
+                        Color = System.Drawing.Color.Green,
+                        ChartType = SeriesChartType.Pie
+                    };
+
+                    chart2.Series.Add(serie1);
+                    serie1.Points.Add(x);
+                    serie1.Points.Add(y);
+                }));
+            }
+            else
+            {
+                chart2.Series.Clear();
+                chart2.BackColor = Color.Transparent;
+                Series serie1 = new Series
+                {
+                    Name = "Grados",
+                    IsVisibleInLegend = false,
+                    Color = System.Drawing.Color.Green,
+                    ChartType = SeriesChartType.Pie
+                };
+
+                chart2.Series.Add(serie1);
+                serie1.Points.Add(x);
+                serie1.Points.Add(y);
+            }
+
+            if (lblActualizacion2.InvokeRequired)
+            {
+                lblActualizacion2.Invoke(new MethodInvoker(delegate
+                {
+                    lblActualizacion2.Text = DateTime.Now.ToString();
+                }));
+            }
+            else
+            {
+                lblActualizacion2.Text = DateTime.Now.ToString();
+            }
+        }
+
+        private void metroLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
